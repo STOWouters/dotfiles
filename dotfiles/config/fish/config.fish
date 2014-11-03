b33f255a09e6ea8b0869ea699cfaecce97b3946f
Stijn-Flipper <w.stijn@gmail.com>
2014-11-03 14:28:48 +0100

    add bunch of dotfiles

---
+++ dotfiles/config/fish/config.fish
@@ -0,0 +1,7 @@
+# set prompt colors
+set -xg fish_color_user cyan
+set -xg fish_color_host yellow
+
+# use virtualfish compatible aliases
+set -g VIRTUALFISH_COMPAT_ALIASES
+source $HOME/Git/virtualfish/virtual.fish
