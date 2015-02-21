bb122d184256751d970e793e2c1ce3d4e241bda3
Stijn-Flipper <w.stijn@gmail.com>
2015-02-21 12:08:15 +0100

    [FIX] Restore fish prompt when colors not set
    Sometimes fish fails to set color, i.e. the command `set | grep fish_color`
    prints nothing, this fix ensures that the colors are set properly. This are the
    default colors as given in
    `/usr/share/fish/functions/__fish_config_interactive.fish`.

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,38 +1,69 @@
-# set prompt colors
-set -xg fish_color_date yellow
-set -xg fish_color_user cyan
-set -xg fish_color_host magenta
-set -xg fish_color_virtualenv blue
+# Regular syntax highlighting colors
+set fish_color_normal normal
+set fish_color_command 005fd7 purple
+set fish_color_param 00afff cyan
+set fish_color_redirection normal
+set fish_color_comment red
+set fish_color_error red --bold
+set fish_color_escape cyan
+set fish_color_operator cyan
+set fish_color_quote brown
+set fish_color_autosuggestion 555 yellow
+set fish_color_valid_path --underline
 
-set -xg __fish_git_prompt_show_informative_status 1
-set -xg __fish_git_prompt_hide_untrackedfiles 1
+set fish_color_cwd green
+set fish_color_cwd_root red
 
-set -xg __fish_git_prompt_color_branch magenta bold
-set -xg __fish_git_prompt_showupstream "informative"
-set -xg __fish_git_prompt_char_upstream_ahead "↑"
-set -xg __fish_git_prompt_char_upstream_behind "↓"
-set -xg __fish_git_prompt_char_upstream_prefix ""
+# Background color for matching quotes and parenthesis
+set fish_color_match cyan
 
-set -xg __fish_git_prompt_char_stagedstate "●"
-set -xg __fish_git_prompt_char_dirtystate "✚"
-set -xg __fish_git_prompt_char_untrackedfiles "…"
-set -xg __fish_git_prompt_char_conflictedstate "✖"
-set -xg __fish_git_prompt_char_cleanstate "✔"
+# Background color for search matches
+set fish_color_search_match --background=purple
 
-set -xg __fish_git_prompt_color_dirtystate blue
-set -xg __fish_git_prompt_color_stagedstate yellow
-set -xg __fish_git_prompt_color_invalidstate red
-set -xg __fish_git_prompt_color_untrackedfiles $fish_color_normal
-set -xg __fish_git_prompt_color_cleanstate green bold
+# Pager colors
+set fish_pager_color_prefix cyan
+set fish_pager_color_completion normal
+set fish_pager_color_description 555 yellow
+set fish_pager_color_progress cyan
+
+# Directory history colors
+set fish_color_history_current cyan
+
+# Prompt colors
+set fish_color_date yellow
+set fish_color_user cyan
+set fish_color_host magenta
+set fish_color_virtualenv blue
+
+set __fish_git_prompt_show_informative_status 1
+set __fish_git_prompt_hide_untrackedfiles 1
+
+set __fish_git_prompt_color_branch magenta bold
+set __fish_git_prompt_showupstream "informative"
+set __fish_git_prompt_char_upstream_ahead "↑"
+set __fish_git_prompt_char_upstream_behind "↓"
+set __fish_git_prompt_char_upstream_prefix ""
+
+set __fish_git_prompt_char_stagedstate "●"
+set __fish_git_prompt_char_dirtystate "✚"
+set __fish_git_prompt_char_untrackedfiles "…"
+set __fish_git_prompt_char_conflictedstate "✖"
+set __fish_git_prompt_char_cleanstate "✔"
+
+set __fish_git_prompt_color_dirtystate blue
+set __fish_git_prompt_color_stagedstate yellow
+set __fish_git_prompt_color_invalidstate red
+set __fish_git_prompt_color_untrackedfiles $fish_color_normal
+set __fish_git_prompt_color_cleanstate green bold
 
 # use virtualfish compatible aliases:
 # https://github.com/adambrenecki/virtualfish
 #
 # only source it when using fish 2.1 or older
-set -xg VIRTUALFISH_COMPAT_ALIASES
+set -x VIRTUALFISH_COMPAT_ALIASES
 if not test $FISH_VERSION = (echo $FISH_VERSION | grep -E "^(1|(2\.0))")
     source virtual.fish
 end
 
 # export PATH
-set -xg PATH $PATH $HOME/bin
+set -x PATH $PATH $HOME/bin
